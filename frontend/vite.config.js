c038ae28d2d8cf54e698201def53e23043bc0ce3
Арина Клюева <arinaklueva@MacBook-Pro-Arina-2.local>
2024-11-26 23:50:33 +0300

    Hello

---
+++ frontend/vite.config.js
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    port: 8954,
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['@meforma/vue-toaster'], // Externalize this module during build if needed
+    },
+  },
+})
